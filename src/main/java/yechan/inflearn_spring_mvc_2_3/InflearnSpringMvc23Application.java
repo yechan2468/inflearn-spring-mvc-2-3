a515736def816739d8a8fe2b46e094fc299c43d2
yechan <yechan24680@gmail.com>
2025-10-08 18:05:32 +0900

    first commit

---
+++ src/main/java/yechan/inflearn_spring_mvc_2_3/InflearnSpringMvc23Application.java
@@ -0,0 +1,13 @@
+package yechan.inflearn_spring_mvc_2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringMvc23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringMvc23Application.class, args);
+	}
+
+}
