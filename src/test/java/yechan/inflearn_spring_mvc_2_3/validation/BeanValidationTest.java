3e6f6a1a281507f6473841f246e7808376513585
yechan <yechan24680@gmail.com>
2025-11-07 17:42:50 +0900

    section5 58

---
+++ src/test/java/yechan/inflearn_spring_mvc_2_3/validation/BeanValidationTest.java
@@ -0,0 +1,28 @@
+package yechan.inflearn_spring_mvc_2_3.validation;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import yechan.inflearn_spring_mvc_2_3.domain.item.Item;
+
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item(" ", 0, 10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation.getMessage() = " + violation.getMessage());
+        }
+    }
+}
